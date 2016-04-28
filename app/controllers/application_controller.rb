c4eec6380e5b18038340dcf445b823ac3001ea92
Marshal Ma <mashuo93@gmail.com>
2016-04-28 01:05:05 -0500

    add field-reason to table submissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+    # Confirms a logged-in user.
+    def current_user
+      
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421035326) do
+ActiveRecord::Schema.define(version: 20160428060109) do
 
   create_table "diseases", force: :cascade do |t|
     t.string   "disease"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160421035326) do
     t.boolean  "is_related"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "reason"
   end
 
   add_index "submissions", ["disease_id"], name: "index_submissions_on_disease_id"
