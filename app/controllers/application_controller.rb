6ff416e87f083253387c10569ce91c131fbb23fc
Ke Peng <pk490625936@gmail.com>
2016-04-29 00:13:03 -0500

    modify application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def reason_to_index(str)
-	if str == "VISA"
+	 if str == "Comprehensive"
 		return 0
-	elsif str == "MasterCard"
+	 elsif str == "Irrelevant Study"
 		return 1
+   elsif str == "Not Enough Experiment"
+    return 2
+   elsif str == "No health Control"
+    return 3
+   elsif str == "Micro rna"
+    return 4
+   elsif str == "Biomarker"
+    return 5
+   elsif str == "Other"
+    return 6
 	end
   end
 
