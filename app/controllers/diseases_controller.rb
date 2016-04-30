106a5c4343dfd44ca113d4b48db58a8a0ad74c27
Marshal Ma <mashuo93@gmail.com>
2016-04-30 00:13:56 -0500

    fixed bug with disease html structure, submit was not in the form tag resulting no response when submitting

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,53 +1,33 @@
 class DiseasesController < ApplicationController
-
-@@dis = []
-
-
   def index
     # byebug
-   $i = 0
-   $num = 5
-   @dis = @@dis
+    num_per_page = 5
+    @diseases_this_page = []
 
-   while $i < $num do
-   	arr = Disease.find(rand(1...19732))
-   	@dis.push(arr)
-   	$i += 1
-   end
-
-   while @dis.length > 5 do
-   	@dis.shift
-   end
+    (0..num_per_page-1).each do
+    	arr = Disease.find(rand(1...Disease.count))
+     	@diseases_this_page << arr
+    end
   end
 
   def import
+    # byebug
+    user_id = session[:user_id]
   	choose = params[:choose]
   	reason = params[:reason]
-  	$user_id = session[:user_id]
-
-    @dis = @@dis
-    @arr = []
+    diseases = params[:dis]
 
     if choose == nil
-      flash[:error] = "No answer given!"
+      flash[:warning] = "No answer given!"
       redirect_to diseases_index_path
       return
     end
 
-    choose.keys.each do |key_choose|
-    	$i = 0
-    	# binding.pry
-    	while $i < @dis.size do
-    		if (key_choose == @dis.at($i).id.to_s)
-    			@arr << @dis.at($i).id
-    			@arr << $user_id
-    			@arr << choose_to_bool(choose[key_choose])
-    			@arr << reason_to_index(reason[key_choose])
-    		end
-    		$i += 1
-    	end
+    choose.keys.each do |d_id|
+      Submission.create!({:disease_id => d_id, :user_id => user_id, :is_related => choose[d_id], :reason => reason[d_id]})
     end
 
-	Submission.insert(@arr)
+    flash[:success] = "Successfully submitted #{choose.keys.size} questions."
+    redirect_to diseases_index_path
   end
 end
